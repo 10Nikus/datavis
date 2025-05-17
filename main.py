036c2321fbc776f84277cd4f932c1ebecf6d1362
justynaoczko <justynaoczko6@gmail.com>
2025-05-17 10:10:50 +0200

    Test

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+'''  interaktywna aplikacja do analizy i wizualizacji danych giełdowych z wykorzystaniem wskaźników technicznych i statystyk opisowych, zawierająca:
+1) import danych z pliku CSV
+2) statystyki opisowe
+3) interaktywną wizualizację danych (wykres/y)
+4) wskaźniki Bollingera '''
+
 df = pd.read_csv('data/sample_stock_data.csv')
 
 print(df["Volume"].median())
