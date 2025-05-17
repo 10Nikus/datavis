3bf21325db038913f4ddd379843c0647d51a1241
justynaoczko <justynaoczko6@gmail.com>
2025-05-17 12:54:38 +0200

    Dodanie integracji z GUI (na ten moment działa tylko dla count

--- count.py
+++ count.py
@@ -1,6 +1,13 @@
 import pandas as pd
+from tkinter import messagebox
 
-df = pd.read_csv('data/sample_stock_data.csv')
-
-print(f"Liczba obserwacji: {df['Volume'].count()}")
-
+def click_fun(win, Mlabel):
+    try:
+        df = pd.read_csv('data/sample_stock_data.csv')
+        count = df['Volume'].count()
+        Mlabel.config(text=f"[ count ] Liczba obserwacji: {count}")
+        print(f"Liczba obserwacji: {count}")
+    except FileNotFoundError:
+        messagebox.showerror("Błąd", "Nie znaleziono pliku: data/sample_stock_data.csv")
+    except Exception as e:
+        messagebox.showerror("Błąd", f"Wystąpił problem: {e}")
--- main.py
+++ main.py
@@ -1,44 +1,81 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-'''  
-    interaktywna aplikacja do analizy i wizualizacji danych giełdowych z wykorzystaniem wskaźników technicznych i statystyk opisowych, zawierająca:
+"""
+  interaktywna aplikacja do analizy i wizualizacji danych giełdowych z wykorzystaniem wskaźników technicznych 
+  i statystyk opisowych, zawierająca:
     1) import danych z pliku CSV
     2) statystyki opisowe
     3) interaktywną wizualizację danych (wykres/y)
     4) wskaźniki Bollingera 
-'''
+"""
 
-df = pd.read_csv('data/sample_stock_data.csv')
+from tkinter import *
+from tkinter import ttk
+from tkinter import constants, messagebox
 
-print("Statystyki opisowe: ")
-print(df.describe())
+zad = 1
+print("Loading zad 1/", zad); zad +=1
+import count as T01
 
-print(f"Liczba obserwacji: {df['Volume'].count()}")
-print(f"Mediana: {df['Volume'].median()}")
-print(f"Odchylenie standardowe: {df['Volume'].std()}")
-print(f"Wartość minimalna: {df['Volume'].min()}")
-print(f"Wartość maksymalna: {df['Volume'].max()}")
-print(f"3 kwartyl (75%): {df['Volume'].quantile(0.75)}")
+print("Loading zad 2/", zad); zad +=1
+# import median as T02
 
-# wskazniki bollingera
-df['SMA20'] = df['Close'].rolling(window=20).mean()
-df['UpperBand'] = df['SMA20'] + 2 * df['Close'].rolling(window=20).std()
-df['LowerBand'] = df['SMA20'] - 2 * df['Close'].rolling(window=20).std()
+print("Loading zad 3/", zad); zad +=1
+# import std as T03
 
-plt.figure(figsize=(14, 6))
-plt.plot(df['Date'], df['Close'], label='Cena zamknięcia', color='blue')
-plt.plot(df['Date'], df['SMA20'], label='Średnia 20-dniowa', color='orange')
-plt.plot(df['Date'], df['UpperBand'], label='Górna banda Bollingera', color='pink')
-plt.plot(df['Date'], df['LowerBand'], label='Dolna banda Bollingera', color='gray')
+print("Loading zad 4/", zad); zad +=1
+# import max_min as T04
 
-plt.title('Cena zamknięcia z wskaźnikami Bollingera')
-plt.xlabel('Data')
-plt.ylabel('Cena')
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-plt.xticks(rotation=45)
-plt.show()
+print("Loading zad 5/", zad); zad +=1
+# import quantile as T05
+
+print("Loading zad 6/", zad); zad +=1
+#import mean as T06
+
+print("Loading zad 6/", zad); zad +=1
+#import bollinger as T06
+
+win = Tk()
+win.geometry("400x200")
+win.minsize(400,200)
+win.maxsize(400,200)
+
+def About():
+  messagebox.showinfo('SuperSTAT', 'Program written by students of 2025 Python introductory class,\nver.: 2025/03/22.1')
+
+def Quit(*event):
+  quit()
+
+def NoModule():
+  messagebox.showinfo('ERROR', 'Module is improperly implemented, run directly')
+
+# options for buttons
+## for pack: button_opt = {'fill': constants.BOTH, 'padx': 15, 'pady': 5}
+grid_opt = {'padx' : 15, 'pady' : 5}
+
+# Create a Label widget
+Mlabel  = Label(win, text="[ Select module ]", font=('Arial', 14))
+Mlabel.grid(row=0, columnspan=5)
+
+MStatus = Label(win, text="...", font=('Arial', 14))
+MStatus.grid(row=5, columnspan=5)
+#Mlabel.pack(pady=40)
+
+# Create commands buttons
+ttk.Button(win, text="[  count   ]", command=lambda: T01.click_fun(win, Mlabel)).grid(row=1, column=0, **grid_opt)
+ttk.Button(win, text="[  median   ]", command=lambda: T02.click_fun(win, Mlabel)).grid(row=1, column=1, **grid_opt)
+ttk.Button(win, text="[  std   ]", command=lambda: T03.click_fun(win, Mlabel)).grid(row=1, column=2, **grid_opt)
+ttk.Button(win, text="[  min max   ]", command=lambda: T04.click_fun(win, Mlabel)).grid(row=2, column=0, **grid_opt)
+ttk.Button(win, text="[  quantile   ]", command=lambda: T05.click_fun(win, Mlabel)).grid(row=2, column=1, **grid_opt)
+ttk.Button(win, text="[  mean   ]", command=lambda: T06.click_fun(win, Mlabel)).grid(row=2, column=2, **grid_opt)
+ttk.Button(win, text="[  bollinger   ]", command=lambda: T07.click_fun(win, Mlabel)).grid(row=3, column=0, **grid_opt)
+ttk.Button(win, text="[  costam   ]", command=lambda: T07.click_fun(win, Mlabel)).grid(row=3, column=1, **grid_opt)
+ttk.Button(win, text="[  costam   ]", command=lambda: T07.click_fun(win, Mlabel)).grid(row=3, column=2, **grid_opt)
 
 
+#=====================
+ttk.Button(win, text="[ O programie: ]", command=About).grid(row=4,column=0, columnspan = 2 )
+ttk.Button(win, text="[    Zamknij   ]", command=Quit).grid(row=4, column=1, columnspan = 2)
+win.bind("<KeyPress-Escape>", Quit)
+win.protocol("WM_DELETE_WINDOW", Quit)
+# start interface:
+win.mainloop()
+## print(f"(Main)data = {data}") # global variable data is known to all madules
