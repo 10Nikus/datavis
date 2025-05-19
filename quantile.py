78f62dfae404cb8e893769c801eb0ea62d3f8310
10Nikus <kijasnikodem@gmail.com>
2025-05-19 12:06:41 +0200

    commit to merge

--- quantile.py
+++ quantile.py
@@ -4,8 +4,9 @@ from tkinter import messagebox
 def click_fun(win, Mlabel):
     try:
         df = pd.read_csv('data/sample_stock_data.csv')
-        quantile = df['Volume'].quantile(0.75)
-        Mlabel.config(text=f"3rd quantile (75%): {quantile}")
+        quantile1 = df['Volume'].quantile(0.25)
+        quantile3 = df['Volume'].quantile(0.75)
+        Mlabel.config(text=f"1rd quantile (25%): {quantile1}\n 3rd quantile (75%): {quantile3}")
 
     except FileNotFoundError:
         messagebox.showerror("Błąd", "Nie znaleziono pliku: data/sample_stock_data.csv")
