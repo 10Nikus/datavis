5a5f5ee1ee219266de4bf6fa88cdd457ef8629e8
justynaoczko <justynaoczko6@gmail.com>
2025-05-20 22:18:01 +0200

    Uzupełnienie programu o Bollinger

--- max_min.py
+++ max_min.py
@@ -1,15 +1,6 @@
 import pandas as pd
-from tkinter import messagebox
-
 
 def click_fun(win, Mlabel, df, name):
-    try:
-        min = df[name].min()
-        max = df[name].max()
-        Mlabel.config(text=f"Minimum: {min} \nMaximum: {max} of {name}")
-
-    except FileNotFoundError:
-        messagebox.showerror("Błąd", "Nie znaleziono pliku: data/sample_stock_data.csv")
-
-    except Exception as e:
-        messagebox.showerror("Błąd", f"Wystąpił problem: {e}")
+    min_val = df[name].min().round(3)
+    max_val = df[name].max().round(3)
+    Mlabel.config(text=f"Minimum: {min_val} \nMaximum: {max_val} of {name}")
