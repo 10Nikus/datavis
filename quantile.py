1d3747474ffe5164bd1df377a9ac59e60313c266
10Nikus <kijasnikodem@gmail.com>
2025-05-24 14:41:40 +0200

    Update quantile display format for improved readability

--- quantile.py
+++ quantile.py
@@ -6,7 +6,7 @@ def click_fun(win, Mlabel, df, name):
     try:
         quantile1 = df[name].quantile(0.25)
         quantile3 = df[name].quantile(0.75)
-        Mlabel.config(text=f"1rd quantile (25%): {quantile1} and 3rd quantile (75%): {quantile3} of {name}")
+        Mlabel.config(text=f"1rd quantile (25%): {quantile1} \n 3rd quantile (75%): {quantile3} of {name}")
 
     except FileNotFoundError:
         messagebox.showerror("Błąd", "Nie znaleziono pliku: data/sample_stock_data.csv")
